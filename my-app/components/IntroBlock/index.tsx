0d3f6989e252c936a2718b11d355dfb02d600447
Gaurav Gupta <gauravgupta@Gauravs-MacBook-Pro.local>
2021-12-30 13:39:54 +1000

    project added

---
+++ my-app/components/IntroBlock/index.tsx
@@ -0,0 +1,3 @@
+export const IntroBlock = () => {
+    return <>this will be into block</>
+    }
--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../styles/globals.css';
+
+import { useEffect } from "react";
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    
+  },[])
   return <Component {...pageProps} />
 }
 
